6eb35c749ac4004f80ed3635b35a0fa72e810c5e
Bettina Steger <hello@bettysteger.com>
2022-08-01 21:15:20 +0200

    fix setting initial cookie

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 class SETTINGS {
   static const title = 'Flutter PWA Wrapper';
-  static const url = 'https://flutter.dev';
+  static const url = 'http://localhost:8000/'; // http://localhost:8000/ test dev
+  static const cookieDomain = 'localhost';
   // set userAgent to prevent 403 Google 'Error: Disallowed_Useragent'
   // @see https://stackoverflow.com/a/69342626/595152
   static const userAgent = "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36";
@@ -44,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: WebView(
         initialUrl: SETTINGS.url,
         javascriptMode: JavascriptMode.unrestricted,
-        initialCookies: [WebViewCookie(name: 'isNative', value: "true", domain: SETTINGS.url.replaceAll('https://', ''))],
+        initialCookies: const [WebViewCookie(name: 'isNative', value: 'true', domain: SETTINGS.cookieDomain)],
         onWebViewCreated: (controller) => webviewController = controller,
         // onPageFinished: (url) => webviewController.runJavascript('localStorage.setItem("isNative", true)'),
         navigationDelegate: (navigation) {
