da79a9ed8cbecc193694cb703130a80e60ab80ba
Bettina Steger <hello@bettysteger.com>
2022-09-12 13:22:50 +0200

    prepare demo

---
+++ demo/sw.js
@@ -0,0 +1,24 @@
+var cacheName = 'pwa';
+var filesToCache = [
+  '/',
+  '/index.html'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
