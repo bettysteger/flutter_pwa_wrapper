000565493596c32192645bb0c46c090bf8cf81ef
Bettina Steger <hello@bettysteger.com>
2025-12-25 19:00:27 +0100

    Retail Beats Academy

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_pwa_wrapper/push_notifications_manager.dart';
 
 class SETTINGS {
-  static const title = 'Innform';
-  static const url = 'https://app.innform.io/'; // test dev
-  static const allowedOrigins = ["innform.io", "intersport.de", "intersportakademie.at", "serverhero.de", "lowa.de", "sebazzo.com"];
+  static const title = 'Retail Beats Academy';
+  static const url = 'https://retailbeatsacademy.eu/'; // test dev
+  static const allowedOrigins = ["innform.io", "retailbeatsacademy.eu"];
   static const newTabs = ["help.innform.io"];
   static const cookieDomain = null; // only necessary if you are using a subdomain and want it on the top-level domain
 
